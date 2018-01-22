9af6f9a82fcc871fe0d71c9edb089521c19024d9
CM Student <cmstudent@UHWO-ACM-C-04.local>
2018-01-22 10:09:29 -1000

    Initial Commit

---
+++ Hackwich2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hackwich2
+//
+//  Created by CM Student on 1/22/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
